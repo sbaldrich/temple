b8fde840511a9033ae459d00bda6d615bf163059
Santiago Baldrich <santiago.baldrich@gmail.com>
2022-05-22 17:24:59 +0200

    comprog - minor improvements to java template

--- comprog/java/{{cookiecutter.classname}}/{{cookiecutter.classname}}.java
+++ comprog/java/{{cookiecutter.classname}}/{{cookiecutter.classname}}.java
@@ -1,64 +1,78 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
+class {{cookiecutter.classname}} {
 
-	public static void main(String[] args){
-		{%- if cookiecutter.comments -%}
-		// Initialize a ContestIO reading from __input.txt. 
-		// If the file doesn't exist, read from stdin.
-		{% endif %}
-		ContestIO io = ContestIO.get("__input.txt");
+    public static void main(String[] args){
+        {%- if cookiecutter.comments == True %}
+        // Initialize a ContestIO reading from __input.txt. 
+        // If the file doesn't exist, read from stdin.
+        {% endif %}
+        ContestIO io = ContestIO.get("__input.txt");
         // Solve here
-		io.out.close();
-	}
+        io.close();
+    }
 
-	static class ContestIO {
-		
-		PrintWriter out = new PrintWriter(System.out);
-		BufferedReader reader;
+    static class ContestIO {
+        
+        PrintWriter out = new PrintWriter(System.out);
+        BufferedReader reader;
 
-		StringTokenizer tokenizer;
+        StringTokenizer tokenizer;
 
-		ContestIO(Reader reader) {
-			this.reader = new BufferedReader(reader);
-		}
+        ContestIO(Reader reader) {
+            this.reader = new BufferedReader(reader);
+        }
 
-		static ContestIO get(){
-			return new ContestIO(new InputStreamReader(System.in));
-		}
+        static ContestIO get(){
+            return new ContestIO(new InputStreamReader(System.in));
+        }
 
-		static ContestIO get(String file){
-			try{
-				return new ContestIO(new FileReader(file));
-			} catch(IOException ex){
-				return ContestIO.get();
-			}
-		}
+        static ContestIO get(String file){
+            try{
+                return new ContestIO(new FileReader(file));
+            } catch(IOException ex){
+                return ContestIO.get();
+            }
+        }
 
-		String readLine(){
-			try {
-				return reader.readLine();
-			} catch (IOException e) {
-				throw new RuntimeException("Couldn't read anything", e);
-			}
-		}
+        String readLine(){
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException("Couldn't read anything", e);
+            }
+        }
 
-		String next(){
-			while(tokenizer == null || ! tokenizer.hasMoreElements()){
-				tokenizer = new StringTokenizer(readLine());
-			}
-			return tokenizer.nextToken();
-		}
+        String next(){
+            while(tokenizer == null || ! tokenizer.hasMoreElements()){
+                tokenizer = new StringTokenizer(readLine());
+            }
+            return tokenizer.nextToken();
+        }
 
-		int nextInt(){
-			return Integer.parseInt(next());
-		}
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
 
-		long nextLong(){
-			return Long.parseLong(next());
-		}
-	}
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+
+        ContestIO printf(final String format, final Object... args){
+            out.printf(format, args);
+            return this;
+        }
+
+        ContestIO println(final Object arg){
+            out.println(arg);
+            return this;
+        }
+
+        void close(){
+            out.close();
+        }
+    }
 
 }
 
